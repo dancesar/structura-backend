c803ba9bdb35e520ba86a530852cd8d9114f9661
Danillo <danillo.yamagush@gmail.com>
2024-11-24 19:06:59 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/structura/StructuraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.structura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/structura/StructuraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.structura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
